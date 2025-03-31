45ef113927d42591d89a3ce2c9912e02dcb82d0e
Hachiq <lesiv433@gmail.com>
2025-03-31 20:48:06 +0300

    add options for time shift

--- client/src/core/components/shifter/shifter.component.ts
+++ client/src/core/components/shifter/shifter.component.ts
@@ -17,6 +17,10 @@ export class ShifterComponent {
     minutes: new FormControl(0, [Validators.min(0), Validators.max(59), Validators.required]),
     seconds: new FormControl(0, [Validators.min(0), Validators.max(59), Validators.required]),
     milliseconds: new FormControl(0, [Validators.min(0), Validators.max(999), Validators.required]),
+    
+    shiftDirection: new FormControl('forward', [Validators.required]),
+    shiftType: new FormControl('add', [Validators.required]),
+    index: new FormControl(1, [Validators.min(1), Validators.required]),
   });
 
   editor = inject(SubEditorService);
@@ -27,9 +31,15 @@ export class ShifterComponent {
     const timeOffsetMs = this.editor.convertTimeToMilliseconds(timeOffset);
     const subtitles = this.subtitlesStorage.getSubtitles();
 
-    if (subtitles) {
-      this.editor.adjustSubtitleTimesFromIndex(subtitles, 0, timeOffsetMs, 'forward', 'add');
+    if (!subtitles) {
+      return;
     }
+
+    const index = this.shiftForm.controls.index.value ?? 1;
+    const direction = (this.shiftForm.controls.shiftDirection.value ?? 'forward') as 'forward' | 'backward';
+    const operation = (this.shiftForm.controls.shiftType.value ?? 'add') as 'add' | 'subtract';
+
+    this.editor.adjustSubtitleTimesFromIndex(subtitles, index - 1, timeOffsetMs, direction, operation);
   }
 
   getTimeOffsetFromControls(): Time {
